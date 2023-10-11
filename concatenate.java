bcd1fc9c307893805f859f498530cc241508130d
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-10-11 18:48:11 +0530

    concate string

--- concatenate.java
+++ concatenate.java
@@ -1,8 +1,6 @@
 public class concatenate {
     public static void main (String[]args)
     {
-        String S1 = "DIVYANSHU";
-        String S2 =  "DHWANI";
         String S3 = S1 + " AND " + S2 ;
         System.out.println(S3);
     }
