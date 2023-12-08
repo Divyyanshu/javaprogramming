dd2d6219e3478373b6ad7f968e723e0f2d53e0a0
divyanshu tailor <128120812+divyyuanshutailor@users.noreply.github.com>
2023-12-09 00:58:24 +0530

    start java

---
+++ javaBasics.java
@@ -0,0 +1,9 @@
+public class javaBasics {
+    public static void main(String args[]) {
+        System.out.print("hello world");
+        System.out.print("world\n");
+        System.out.print("hello\n");
+        System.out.print("divyanshu");
+        System.out.print("hello world");
+    }
+}
